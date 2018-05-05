55aa85050c7610b00fe0c849e19cbc288048112e
Ehirim Emeka <bluegrayonics@gmail.com>
2018-05-05 18:25:07 +0100

    Added reccipes and store components

--- src/app/dashboard/setup/setup-routing.ts
+++ src/app/dashboard/setup/setup-routing.ts
@@ -1,6 +1,9 @@
+
+import {CustomersComponent} from './customers/customers.component'
+import {MaterialsComponent} from './materials/materials.component';;
+import { RecipesComponent } from './recipes/recipes.component';
+import { StoreComponent } from './store/store.component';
 import {VendorsComponent} from './vendors/vendors.component';
-import {CustomersComponent} from './customers/customers.component';
-import {MaterialsComponent} from './materials/materials.component';
 import {SetupHomeComponent} from './setup-home/setup-home.component';
 
 import {Routes} from '@angular/router';
@@ -8,8 +11,11 @@ import {Routes} from '@angular/router';
 
 export const SETUP_ROUTES: Routes = [
     {path: '', component: SetupHomeComponent},
-    {path: 'vendors', component: VendorsComponent},
     {path: 'customers', component: CustomersComponent},
-    {path: 'materials', component: MaterialsComponent}
+    {path: 'materials', component: MaterialsComponent},
+    {path: 'recipes', component: RecipesComponent},
+    {path: 'store', component: StoreComponent},
+    {path: 'vendors', component: VendorsComponent},
+
 
 ];
--- src/app/dashboard/setup/setup.module.ts
+++ src/app/dashboard/setup/setup.module.ts
@@ -6,13 +6,15 @@ import {MaterialsComponent} from './materials/materials.component';
 import {SetupHomeComponent} from './setup-home/setup-home.component';
 import {RouterModule} from '@angular/router';
 import {SETUP_ROUTES} from './setup-routing';
+import { RecipesComponent } from './recipes/recipes.component';
+import { StoreComponent } from './store/store.component';
 
 @NgModule({
     imports: [
         CommonModule,
         RouterModule.forChild(SETUP_ROUTES)
     ],
-    declarations: [VendorsComponent, CustomersComponent, MaterialsComponent, SetupHomeComponent],
+    declarations: [VendorsComponent, CustomersComponent, MaterialsComponent, SetupHomeComponent, RecipesComponent, StoreComponent],
     exports: [VendorsComponent, CustomersComponent, MaterialsComponent, SetupHomeComponent, RouterModule]
 })
 export class SetupModule {
